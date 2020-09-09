675e2adea30a4d27f644cc87015ac498f587bf42
666hesoyam <56505654+666hesoyam@users.noreply.github.com>
2020-09-09 22:42:45 +0300

    finaly fixed small bags

--- HTML/js/components/slick.main.js
+++ HTML/js/components/slick.main.js
@@ -1,20 +1,10 @@
-// banners on index.html
+// banners on index.html and links on flight-search.html
 $(document).ready(function(){
     $('.slider').slick({
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 3000,
         dots: true,
         slidesToShow: 1,
-        // prevArrow: "<img src='img/left-arrow.svg' class='prev' alt='prev'>",
-        // nextArrow: "<img src='img/right-arrow.svg' class='next' alt='next'>",
         slidesToScroll: 1
-        // responsive: [
-        //                 {
-        //                     breakpoint: 900,
-        //                     settings: {
-        //                     slidesToShow: 1
-        //                     }
-        //                 },
-        //             ]
     });
 });
